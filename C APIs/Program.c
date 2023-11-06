433044b2ce46561bfe453d9a69727b0102a3c037
Ngo Ninh <vilesport@gmail.com>
2023-11-06 20:40:01 +0700

    Find bug in description for APIs
    Tested and found that str API not work as expect

--- C APIs/Program.c
+++ C APIs/Program.c
@@ -2,8 +2,22 @@
 #include <string.h>
 
 int main() {
-    char str1[] = "memmove can be very useful......";
-    memmove(str1 + 20, str1 + 15, 11);
-    printf("%s\n", str1);
-    return 0;
+    char str1[] = "abcde";
+    char str2[] = "fgh";
+
+    printf("%s\n%s\n", str1, str2);
+
+    int cases = strcmp(str1, str2);
+
+    if(cases < 0)
+    {
+        char str3[] = "";
+        strcpy(str3, str1);
+        printf("%s\n", str3);
+        strcpy(str1, str2);
+        printf("%s\n", str1);
+        strcpy(str2, str3);
+        printf("%s\n%s\n", str1, str2);
+        return 0;
+    }
 }
