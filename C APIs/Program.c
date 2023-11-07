f850187a48accebbcb9eb0d46eddffdcd3638ea1
Ngo Ninh <vilesport@gmail.com>
2023-11-07 22:03:19 +0700

    Fix description bug

--- C APIs/Program.c
+++ C APIs/Program.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main() {
-    char str1[] = "abcde";
-    char str2[] = "fgh";
-
-    printf("%s\n%s\n", str1, str2);
-
-    int cases = strcmp(str1, str2);
-
-    if(cases < 0)
-    {
-        char str3[] = "";
-        strcpy(str3, str1);
-        printf("%s %s\n", str3, str2);
-        strcpy(str1, str2);
-        printf("%s\n", str1);
-        strcpy(str2, str3);
-        printf("%s\n%s\n", str1, str2);
-        return 0;
-    }
+    char *str = malloc(8 * (sizeof(char)));
+    str[0] = 'a';
+    str[1] = '0';
+    printf("%c", *(str + 1));
 }
