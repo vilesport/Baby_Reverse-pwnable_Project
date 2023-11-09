de97641af0d2d6be809433202b0e4daf668b9fe6
vilesport <vilesport@gmail.com>
2023-11-09 18:24:43 +0700

    Add new API founded
    + Add strstr() API

---
+++ C APIs/Report for APIs/str/strstr().c
@@ -0,0 +1,9 @@
+//Declaration:
+	char *strstr (const char *s1, const char *s2)
+/*
++ s1: xâu ban đầu
++ s2: xâu cần search
+
+Description:
+	strstr() sẽ trả về con trỏ nơi tìm được xâu s2 trong xâu s1 nên chỉ cần có s2 trong s1 thì sẽ value return != 0
+*/
