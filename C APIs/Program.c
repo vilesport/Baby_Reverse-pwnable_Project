0c8cc60aed6de6db6598b5c5ea87993ef1130915
vilesport <vilesport@gmail.com>
2023-11-17 17:03:29 +0700

    Small update
    Update some small details

--- C APIs/Program.c
+++ C APIs/Program.c
@@ -2,11 +2,5 @@
 #include <string.h>
 
 int main() {
-    char str1[] = "abdef";
-    char str2[] = "cd";
-    char str3[] = "lmao";
-    int x = 6;
-    int *k = &x;
-    memmove(str1, str3, *k);
-    printf("%s\n%s\n%s", str1, str2, str3);
+    
 }
---
+++ pwn.college/Note.md
@@ -0,0 +1,1 @@
+* I wanna jump directly into shellcode injection course but i guess i have to learn sth before i can go
